ff9cadc3d3c5379a9c7b6a69374e20cff8c873ee
anand1125kumar <anand1125kumar@gmail.com>
2021-01-28 20:20:43 +0530

    init

--- SmartInsuranceCompany.py
+++ SmartInsuranceCompany.py
@@ -645,12 +645,13 @@ class appNumberIntentHandler(AbstractRequestHandler):
               
                 except BaseException as e:
                     print(e)
-                    raise(e) 
-            else:
-                   speakText = "Please enter valid username and pin for successfull login."
+                    raise(e)
 
-        else:
-               speakText = "Please enter valid username and pin for successfull login."
+                else:
+                    speakText = "Please enter valid username and pin for successfull login."
+
+            else:
+                speakText = "Please enter valid username and pin for successfull login."
         
         handler_input.response_builder.speak(speakText).set_should_end_session(False)
         return handler_input.response_builder.response

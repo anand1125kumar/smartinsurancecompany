25329ad2eab942b8bed4133ce36315a7802b4352
anand1125kumar <anand1125kumar@gmail.com>
2021-01-31 16:31:19 +0530

    init

--- SmartInsuranceCompany.py
+++ SmartInsuranceCompany.py
@@ -649,7 +649,7 @@ class captureunderwritingsIntentHandler(AbstractRequestHandler):
                 raise(e)    
 
                 uwrquest = data1['Item']['uwrquest1']
-                uwrquest = str(uwrquest)
+                #uwrquest = str(uwrquest)
 
                 speakText = uwrquest
 
@@ -663,7 +663,7 @@ class captureunderwritingsIntentHandler(AbstractRequestHandler):
                         'username': username
                         },
                         UpdateExpression="set tempfield=:ca",
-                        ExpressionAttributeValues={':ca': 'uwrquestion1'}         
+                        ExpressionAttributeValues={':ca': 'uwrquest1'}         
                                                 
                     )
 

c44aa38ea85eec95b387650fa37e4743b412be99
anand1125kumar <anand1125kumar@gmail.com>
2021-01-31 16:23:31 +0530

    init'

--- SmartInsuranceCompany.py
+++ SmartInsuranceCompany.py
@@ -790,29 +790,8 @@ class answerIntentHandler(AbstractRequestHandler):
                     raise(e)
 
 
-                #speakText = "underwriting question 2."
-
-                for i in range(2,9):
-                    try:
-                        dynamodb = boto3.resource('dynamodb')
-                        table = dynamodb.Table('Policy_Details')
-                        data1 = table.get_item(
-                                Key={
-                                        'username': username
-                                    }
-                                )
-              
-                    except BaseException as e:
-                        print(e)
-                        raise(e)    
-
-                    uwrans = data1['Item']['uwrans'+i]
-                    uwrans = str(uwrans)
-                    if(uwrans == 'null'):
-                        uwrquest = uwrans = data1['Item']['uwrquest'+i]
-                        uwrquest = str(uwrquest)
-                        speakText = uwrquest
-                        break                   
+                speakText = "underwriting question 2."
+                
 
         else:
             speakText = "Please enter valid username and pin for successfull login."               

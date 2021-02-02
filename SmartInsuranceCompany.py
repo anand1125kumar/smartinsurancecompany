d0c107b338931abd3c7bf00e73d2b2eba9586bc2
anand1125kumar <anand1125kumar@gmail.com>
2021-02-02 15:46:47 +0530

    init

--- SmartInsuranceCompany.py
+++ SmartInsuranceCompany.py
@@ -1509,16 +1509,16 @@ class LogoutIntentHandler(AbstractRequestHandler):
         
 
         try:
-                dynamodb = boto3.resource('dynamodb')
-                table = dynamodb.Table('Temp')
-                data = table.update_item(
-                    Key={
-                        'username': username
-                        },
-                        UpdateExpression="set status=:ca",
-                        ExpressionAttributeValues={':ca': 'false'}         
+            dynamodb = boto3.resource('dynamodb')
+            table = dynamodb.Table('Temp')
+            data = table.update_item(
+                  Key={
+                    'username': username
+                    },
+                    UpdateExpression="set status=:ca",
+                    ExpressionAttributeValues={':ca': 'false'}         
                                                 
-                    )
+                )
 
         except BaseException as e:
             print(e)
